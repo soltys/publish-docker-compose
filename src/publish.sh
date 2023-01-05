88e333fbb0e4b6f37cf3d22b2f0252a5ae2de830
Paweł Sołtysiak <pawel@soltysiak.it>
2023-01-05 17:22:29 +0100

    v0.3.0 removed default docker-compose.yml file path

--- src/publish.sh
+++ src/publish.sh
@@ -1,14 +1,14 @@
 VERSION="$1"
-OVERRIDE="$2"
+DOCKER_COMPOSE="$2"
 REPO_TOKEN="$3"
 GITHUB_REPOSITORY=$(echo "$GITHUB_REPOSITORY" | awk '{print tolower($0)}')
 
 echo "VERSION=$VERSION"
-echo "OVERRIDE=$OVERRIDE"
+echo "DOCKER_COMPOSE=$DOCKER_COMPOSE"
 
 docker login ghcr.io -u "${GITHUB_REF}" -p "${REPO_TOKEN}"
 
-VERSION=$VERSION docker-compose -f docker-compose.yml -f "$OVERRIDE" up --no-start --remove-orphans
+VERSION=$VERSION docker-compose -f "$DOCKER_COMPOSE" up --no-start --remove-orphans
 IMAGES=$(docker inspect --format='{{.Image}}' "$(docker ps -aq)")
 
 echo "IMAGES: $IMAGES"
